0a47e995217322c71fe07dd500c8b233f172c776
Vijay Jaiwal <146645694+VIJAY-989060@users.noreply.github.com>
2025-12-06 22:51:53 +0530

    Update response message in API endpoint

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/api", (req, res) => {
-  res.json({ message: "Hello from Backend!" });
+  res.json({ message: "Hello from Backend Successfull!" });
 });
 
 app.listen(8080, () => {
