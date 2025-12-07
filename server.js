6d237afb6bee4cefa7148b85cabc25b54a5cc817
Vijay Jaiwal <146645694+VIJAY-989060@users.noreply.github.com>
2025-12-07 19:11:40 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/api", (req, res) => {
-  res.json({ message: "Hello from Backend Successfully deployed! -07-12-2025" });
+  res.json({ message: "Hello from Backend Successfully deployed! -07-12-2025-evening" });
 });
 
 app.listen(8080, () => {
